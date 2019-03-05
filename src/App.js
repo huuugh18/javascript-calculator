11e2569d7e7bf0eb907d40c759abf077e6a3d8cb
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-03-05 10:10:28 -0700

    add footer chip linked to github page change background image

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import  Calculator from './Calculator'
 
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Paper id="title">
-          <Typography variant="h3">
-            Calculon
+        <Paper id='title'>
+          <Typography variant='h3' align='center'>
+            CALCULON
           </Typography>
         </Paper>
-        <Calculator />          
+        <Calculator />
+        <div id='footer'>
+          <Chip
+            label={`A freeCodeCamp project by Hugh O'Neill`}
+            href='https://github.com/huuugh18'
+            component='a'
+            clickable
+          />
+        </div>          
       </div>
     );
   }
